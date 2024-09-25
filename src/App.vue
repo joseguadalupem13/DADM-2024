996a1679a2f193a8fdb25f7f3fa07c8297749a08
José Morales <159676530+joseguadalupem13@users.noreply.github.com>
2024-09-25 18:29:57 +0000

    ⭐FEAT: APP vue Add an input text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 const header = ref('App lista de compras');
+const items = ref([
+  {id: '0', label: '10 bolillos'},
+  {id: '1', label: '1 lata frijoles'},
+  {id: '2', label: '1 Chela'},
+  {id: '3', label: '1 Nutela'}
+]);
 
+const newitem = ref('');
 </script>
 
 <template>
@@ -9,7 +16,14 @@ const header = ref('App lista de compras');
     <i class="material-icons shopping-cart-icon"> local_mall </i>
       {{ header }}
   </h1>
+  <input v-model="newItem" type="text" 
+  placeholder="Agregar Articulo"> {{ newItem }}
 
+  <ul>
+    <li 
+      v-for="({ label, id }, i) in items" :key="id"> 
+        {{ i+1 }} {{ i%2==0?'🔥':'🛍️' }} {{ label }}</li>
+  </ul>
   </template>
 
 <style scoped>
